4707d03a44a6c3afdcecdf93c8c04049fa0ece87
augustinebest <augustusbest00@gmaile.com>
2022-10-14 08:40:22 +0100

    Created Footer Component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,20 @@
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div>
+        © 2018 Electro Store. All rights reserved | Design by{" "}
+        <a
+          className="ft-info"
+          target="_blank"
+          href="https//github.com/augustinebest"
+        >
+          Augustine Igwe
+        </a>
+        .
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
